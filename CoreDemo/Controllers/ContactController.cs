063c5d9fcd5ed0f0d0efa03d759eb45c17537fe5
hmaf <maftohi3@gmail.com>
2021-10-30 20:21:26 +0330

    add_contact_us

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramwork;
+using EntityLayer.Concrete;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm=new ContactManager(new EFContactRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContractDate = DateTime.Parse(DateTime.Now.ToString());
+            p.ContractStatus = true;
+            cm.ContactAdd(p);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
