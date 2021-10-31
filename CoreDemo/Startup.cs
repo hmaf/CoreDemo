63919b00ce6087cd68a20b05e6f23786afb3f010
hmaf <maftohi3@gmail.com>
2021-10-31 18:58:25 +0330

    Edit_Layout

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -66,7 +66,7 @@ namespace CoreDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }
