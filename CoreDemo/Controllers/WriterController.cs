c4743a9d56611799fc3c93ea21398863d14b34c5
hmaf <maftohi3@gmail.com>
2021-11-02 17:06:17 +0330

    WriterPanel

---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoreDemo.Controllers
+{
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+        
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
