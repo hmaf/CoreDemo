b017bf0935e450c8bfa6a812ed60e7645df2a220
hmaf <maftohi3@gmail.com>
2021-11-26 20:24:06 +0330

    end

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -3,12 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramwork;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 
 namespace CoreDemo.Controllers
 {
     public class WriterController : Controller
     {
+        WriterManeger wm=new WriterManeger(new EFWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -37,6 +41,22 @@ namespace CoreDemo.Controllers
         public IActionResult WriterFooterPartial()
         {
             return PartialView();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var profile = wm.TGetById(1);
+            return View(profile);
+        }
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            if (!ModelState.IsValid)
+                return View(p);
+
+            wm.Update(p);
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # CoreDemo
 Asp.Net Core Mvc N'Tir Architect
-
-Gorev 3
-
-
-![1](https://user-images.githubusercontent.com/63851483/141655779-de9f6409-3521-4539-94e9-eea7873539ee.png)
-
+sdfsdfsdfsdfs
