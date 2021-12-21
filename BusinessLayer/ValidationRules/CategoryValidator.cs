f4f212517307e02a1636a5d1d8be7fc5f2390e6a
hmaf <maftohi3@gmail.com>
2021-12-21 13:17:36 +0330

    Admin_AddCategory

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+public    class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("لطفا نام را پر کنید")
+                .MaximumLength(50).WithMessage("نمی توانید بیشتر از 50 کاراکتر استفاده کنید")
+                .MinimumLength(2).WithMessage("نام باید از 2 کارکتر بزرگتر باشد");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("لطفا توضیحات را پر کنید");
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramwork;
+using EntityLayer.Concrete;
+using X.PagedList;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager cm=new CategoryManager(new EFCategoryRepository());
+        public IActionResult Index(int page=1)
+        {
+            var result = cm.GetList().ToPagedList(page, 2);
+            return View(result);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            if (!ModelState.IsValid)
+                return View(category);
+            
+            cm.Add(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
+//get current url address
+//var url1 = HttpContext.Request.Path.ToString().Split("/");
