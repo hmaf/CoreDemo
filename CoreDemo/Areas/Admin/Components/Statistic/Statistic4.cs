9068c461baf13c3fb1520350d1ee27260282cebd
hmaf <maftohi3@gmail.com>
2022-01-04 19:23:34 +0330

    static4

---
+++ CoreDemo/Areas/Admin/Components/Statistic/Statistic4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Components.Statistic
+{
+    public class Statistic4:ViewComponent
+    {
+        Context _context=new Context();
+        public IViewComponentResult Invoke()
+        {
+            var values= _context.Admins.SingleOrDefault(x => x.AdminID == 1);
+            ViewData["v1"] = values.Name;
+            ViewData["v2"] = values.ImageURL;
+            ViewData["v3"] = values.ShortDescrioption;
+            return View();
+        }
+    }
+}
--- DataAccessLayer/Migrations/20220104142502_Mig_AddAdminTable.Designer.cs
+++ DataAccessLayer/Migrations/20220104142502_Mig_AddAdminTable.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220104142502_Mig_AddAdminTable")]
+    partial class Mig_AddAdminTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
