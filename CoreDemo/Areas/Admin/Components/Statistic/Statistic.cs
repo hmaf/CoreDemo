8e67187f348f9806ce5609d3bbf60d90ca25d052
hmaf <maftohi3@gmail.com>
2022-01-04 20:34:33 +0330

    Weather_Api

--- CoreDemo/Areas/Admin/Components/Statistic/Statistic.cs
+++ CoreDemo/Areas/Admin/Components/Statistic/Statistic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramwork;
@@ -18,7 +19,16 @@ namespace CoreDemo.Areas.Admin.Components.Statistic
             ViewData["V1"] = bm.GetList().Count;
             ViewData["V2"] = c.Contacts.Count();
             ViewData["V3"] = c.Comments.Count();
+            ViewData["V4"] = WeatherApi().Value;
+
             return View();
+        }
+        private XAttribute WeatherApi(string city = "Tabriz")
+        {
+            string apiKey = "282a4e118584e93c964a2d3774ef42c6";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=xml&units=metric&appid=" + apiKey;
+            XDocument document = XDocument.Load(connection);
+            return document.Descendants("temperature").ElementAt(0).Attribute("value");
         }
     }
 }
